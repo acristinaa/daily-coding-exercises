1f5e9551a10816e2345df575bffed5477453a058
acristinaa <crisstinaayala777@gmail.com>
2025-06-11 16:23:00 +0200

    Problem 4.3

--- stacksAndQueues4.3.py
+++ stacksAndQueues4.3.py
@@ -6,4 +6,14 @@ k = 3
 q = deque() #stores indices of useful elements
 
 for i in range(len(arr)):
-    q.append(i)
+    if q and q[0] < i - k + 1: #Remove indices from the front if they are outside the current window
+        q.popleft()
+
+    while q and arr[i] > arr[q[-1]]: #Remove indices from the back while the current element is greater than the elements they point to, because those elements will never be the max if the current one is larger
+        q.pop()
+
+
+    q.append(i) #Add the current index
+
+    if i >= k - 1: #Once the first full window is formed, print the maximum (at the front of the deque)
+        print(arr[q[0]])
